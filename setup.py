1bfabbaf1ad6587d13cdadf3b7673b9b946fc935
Tom George <tomgeorge1@btinternet.com>
2022-07-28 00:28:25 +0900

    setup bug

--- setup.py
+++ setup.py
@@ -1,15 +1,11 @@
 import setuptools
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
 setuptools.setup(
     name="ratinabox",
     version="0.1",
     description="RatInABox: A package for simulating motion and ephys data in continuous environments",
     author="Tom George",
     author_email="tomgeorge1@btinternet.com",
-    long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/TomGeorge1234/RatInABox",
     download_url="https://github.com/TomGeorge1234/RatInABox",
