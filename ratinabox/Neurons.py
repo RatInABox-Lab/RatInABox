1dc3a1aeef8e00df8ee3d546031e10f51202729d
Pei Huang <hphuang@ibms.sinica.edu.tw>
2022-12-01 14:37:04 +0800

    add GridCells non-random phase offsets

--- ratinabox/Neurons.py
+++ ratinabox/Neurons.py
@@ -690,6 +690,7 @@ class GridCells(Neurons):
             "gridscale": 0.45,
             "random_orientations": True,
             "random_gridscales": True,
+            "random_phase_offsets": True,
             "min_fr": 0,
             "max_fr": 1,
             "name": "GridCells",
@@ -703,7 +704,10 @@ class GridCells(Neurons):
         assert (
             self.Agent.Environment.dimensionality == "2D"
         ), "grid cells only available in 2D"
-        self.phase_offsets = np.random.uniform(0, self.gridscale, size=(self.n, 2))
+        if self.random_phase_offsets == True:
+            self.phase_offsets = np.random.uniform(0, self.gridscale, size=(self.n, 2))
+        else:
+            self.phase_offsets = self.set_phase_offsets()
         w = []
         for i in range(self.n):
             w1 = np.array([1, 0])
@@ -758,6 +762,22 @@ class GridCells(Neurons):
             firingrate * (self.max_fr - self.min_fr) + self.min_fr
         )  # scales from being between [0,1] to [min_fr, max_fr]
         return firingrate
+
+    def set_phase_offsets(self):
+        n_x = int(np.sqrt(self.n))
+        n_y = self.n // n_x
+        n_remaining = self.n - n_x * n_y
+
+        dx = self.gridscale / n_x
+        dy = self.gridscale / n_y
+
+        grid = np.mgrid[(0 + dx/2):(self.gridscale - dx/2):(n_x*1j), (0 + dy/2):(self.gridscale - dy/2):(n_y*1j)]
+        grid = grid.reshape(2, -1).T
+        remaining = np.random.uniform(0, self.gridscale, size=(n_remaining, 2))
+
+        all_offsets = np.vstack([grid, remaining])
+
+        return all_offsets
 
 
 class BoundaryVectorCells(Neurons):
