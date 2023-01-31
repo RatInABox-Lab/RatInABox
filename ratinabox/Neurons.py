113f2fa7aec6c6b9ef548752f29eec859d746400
Tom George <tomgeorge1@btinternet.com>
2023-01-31 18:34:50 +0000

    ObjectVectorCells (new class) added

--- ratinabox/Neurons.py
+++ ratinabox/Neurons.py
@@ -932,14 +932,14 @@ class BoundaryVectorCells(Neurons):
     def get_state(self, evaluate_at="agent", **kwargs):
         """Here we implement the same type if boundary vector cells as de Cothi et al. (2020), who follow Barry & Burgess, (2007). See equations there.
 
-        The way I do this is a little complex. I will describe how it works from a single position (but remember this can be called in a vectorised manner from an arary of positons in parallel)
-            1. An array of normalised "test vectors" span, in all directions at 1 degree increments, from the position
+        The way we do this is a little complex. We will describe how it works from a single position (but remember this can be called in a vectorised manner from an array of positons in parallel)
+            1. An array of normalised "test vectors" span, in all directions at small increments, from the current position
             2. These define an array of line segments stretching from [pos, pos+test vector]
             3. Where these line segments collide with all walls in the environment is established, this uses the function "utils.vector_intercepts()"
             4. This pays attention to only consider the first (closest) wall forawrd along a line segment. Walls behind other walls are "shaded" by closer walls. Its a little complex to do this and requires the function "boundary_vector_preference_function()"
             5. Now that, for every test direction, the closest wall is established it is simple a process of finding the response of the neuron to that wall at that angle (multiple of two gaussians, see de Cothi (2020)) and then summing over all the test angles.
 
-        We also apply a check in the middle to utils.rotate teh reference frame into that of the head direction of the agent iff self.reference_frame='egocentric'.
+        We also apply a check in the middle to utils.rotate the reference frame into that of the head direction of the agent iff self.reference_frame='egocentric'.
 
         By default position is taken from the Agent and used to calculate firing rates. This can also by passed directly (evaluate_at=None, pos=pass_array_of_positions) or ou can use all the positions in the environment (evaluate_at="all").
         """
@@ -1108,6 +1108,135 @@ class BoundaryVectorCells(Neurons):
             ax[i].set_yticks([])
 
         return fig, ax
+
+
+class ObjectVectorCells(Neurons):
+    """
+
+    """
+    def __init__(self, Agent, params={}):
+        """Initialises ObjectVectorCells(), takes as input a parameter dictionary. Any values not provided by the params dictionary are taken from a default dictionary below.
+
+        Each object vector cell has a preferred tuning_distance and tuning_angle. Only when the angle is (with gaussian spread) close to this distance and angle away from the OVC wll the cell fire. 
+
+        It is possible for these cells to be "field_of_view" in which case the cell fires iff the agent is looking towards it. Essentially this is an egocentric OVC with tuning angle set to zero (head on). 
+
+        default_params = {
+            "n": 10,
+            "min_fr": 0,
+            "max_fr": 1,
+            "name": "ObjectVectorCell",
+            "walls_occlude":True, #whether walls occuled OVC firing
+            "field_of_view":False, #set to true for "field of view" OVC
+            "object_locations":None, #otherwise random across Env, the length of this will overwrite "n" 
+            "angle_spread_degrees":15, #can be an array, one for each object, spread of von Mises angular preferrence functinon for each OVC
+            "pref_object_dist": 0.25, #can be an array, one for each object, otherwise randomly drawn from a Rayleigh with this sigma. How far away from OVC the OVC fires. 
+            "xi": 0.08, #parameters determining the distance preferrence function std given the preferred distance. See BoundaryVectorCells or de cothi and barry 2020
+            "beta": 12,
+        }
+        """
+        default_params = {
+            "n": 10,
+            "min_fr": 0,
+            "max_fr": 1,
+            "name": "ObjectVectorCell",
+            "walls_occlude":True, 
+            "field_of_view":False,
+            "object_locations":None, 
+            "angle_spread_degrees":15, 
+            "pref_object_dist":0.25, 
+            "xi": 0.08,
+            "beta": 12,
+        }
+        self.Agent = Agent
+        default_params.update(params)
+        self.params = default_params
+        super().__init__(Agent, self.params)
+
+        assert (self.Agent.Environment.dimensionality == "2D"), "object vector cells only possible in 2D"
+        
+        if self.object_locations is None: 
+            self.object_locations = self.Agent.Environment.sample_positions(self.n)
+            print(f"No object locations passed so {self.n} object locations have been randomly sampled across the environment")
+        
+        #preferred distance and angle to objects and their tuning widths (set these yourself if needed)
+        self.tuning_angles = np.random.uniform(0, 2 * np.pi, size=self.n)
+        self.tuning_distances = np.random.rayleigh(scale=self.pref_object_dist, size=self.n)
+        self.sigma_distances = self.tuning_distances / self.beta + self.xi
+        self.sigma_angles = np.array([(self.angle_spread_degrees / 360) * 2 * np.pi] * self.n)
+
+        if self.field_of_view == True:
+            self.tuning_angles = np.zeros(self.n)
+
+        if self.walls_occlude == True: self.wall_geometry = 'line_of_sight'
+        else: self.wall_geometry = 'euclidean'
+
+        # normalises activity over the environment 
+        locs = self.Agent.Environment.discretise_environment(dx=0.04)
+        locs = locs.reshape(-1, locs.shape[-1])
+
+        if verbose is True:
+            print(
+                "ObjectVectorCells (OVCs) successfully initialised. You can also manually set their orientation preferences (OVCs.tuning_angles, OVCs.sigma_angles), distance preferences (OVCs.tuning_distances, OVCs.sigma_distances)."
+            )
+        return
+
+    def get_state(self, evaluate_at="agent", **kwargs):
+        """Returns the firing rate of the ObjectVectorCells.
+
+        The way we do this is a little complex. We will describe how it works from a single position to a single OVC (but remember this can be called in a vectorised manner from an array of positons in parallel and there are in principle multiple OVCs)
+            1. A vector from the position to the OVC is calculated. 
+            2. The bearing of this vector is calculated and its length. Note if self.field_of_view == True then the bearing is relative to the heading direction of the agent (along its current velocity), not true-north.
+            3. Since the distance to the OVC is calculated taking the environment into account if there is a wall occluding the agent from the obvject this object will not fire. 
+            4. It is now simple to calculate the firing rate of the cell. Each OVC has a preferred distance and angle away from it which cause it to fire. Its a multiple of a gaussian (distance) and von mises (for angle) which creates teh eventual firing rate. 
+
+        By default position is taken from the Agent and used to calculate firing rates. This can also by passed directly (evaluate_at=None, pos=pass_array_of_positions) or you can use all the positions in the environment (evaluate_at="all").
+
+        Returns:
+            firingrates: an array of firing rates
+        """
+        if evaluate_at == "agent":
+            pos = self.Agent.pos
+        elif evaluate_at == "all":
+            pos = self.Agent.Environment.flattened_discrete_coords
+        else:
+            pos = kwargs["pos"]
+        pos = np.array(pos)
+        pos = pos.reshape(-1, pos.shape[-1]) #(N_pos, 2)
+        N_pos = pos.shape[0]
+        N_cells = self.n
+
+        (distances_to_OVCs, vectors_to_OVCs) = self.Agent.Environment.get_distances_between___accounting_for_environment(self.object_locations,pos,return_vectors=True,wall_geometry=self.wall_geometry,) #(N_cells,N_pos) (N_cells,N_pos, 2)
+        flattened_vectors_to_OVCs = vectors_to_OVCs.reshape(-1,2) #(N_pos x N_cells, 2)
+        bearings_to_OVCs = utils.get_angle(flattened_vectors_to_OVCs,is_array=True).reshape(N_cells,N_pos) #(N_cells,N_pos)
+        if self.field_of_view == True: 
+            if evaluate_at == "agent":
+                vel = self.Agent.velocity
+            elif 'vel' in kwargs.keys():
+                vel = kwargs["vel"]
+            else:
+                vel = np.array([1,0])
+                print("Field of view OVCs require a velocity vector but none was passed. Using [1,0]")
+            head_bearing = utils.get_angle(vel)
+            bearings_to_OVCs -= head_bearing
+
+        tuning_distances = np.tile(np.expand_dims(self.tuning_distances,axis=-1),reps=(1,N_pos)) #(N_cell,N_pos)
+        sigma_distances = np.tile(np.expand_dims(self.sigma_distances,axis=-1),reps=(1,N_pos)) #(N_cell,N_pos)
+        tuning_angles = np.tile(np.expand_dims(self.tuning_angles,axis=-1),reps=(1,N_pos)) #(N_cell,N_pos)
+        sigma_angles = np.tile(np.expand_dims(self.sigma_angles,axis=-1),reps=(1,N_pos)) #(N_cell,N_pos)
+
+        firingrate = utils.gaussian(
+            distances_to_OVCs, tuning_distances, sigma_distances, norm=1
+        ) * utils.von_mises(
+            bearings_to_OVCs, tuning_angles, sigma_angles, norm=1
+        ) #(N_cell,N_pos)
+        firingrate = (
+            firingrate * (self.max_fr - self.min_fr) + self.min_fr
+        )  # scales from being between [0,1] to [min_fr, max_fr]
+        return firingrate
+
+
+
 
 class HeadDirectionCells(Neurons):
     """The HeadDirectionCells class defines a population of head direction cells. This class is a subclass of Neurons() and inherits it properties/plotting functions.
